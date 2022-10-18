59c07fb785f162ff9986055635b86739a12e5c88
VVD <daovo739@gmail.com>
2022-10-18 16:29:35 +0700

    fix UI footer, layout

--- src/layouts/Footer/index.js
+++ src/layouts/Footer/index.js
@@ -7,65 +7,62 @@ import {
     GitHub,
 } from '@mui/icons-material/'
 import { Link } from 'react-router-dom'
+
 function Footer() {
     return (
-        <div className={clsx(styles.body)}>
-            <footer>
-                <div className="container">
-                    <div className={clsx('row', 'justify-content-center')}>
-                        <div className={clsx('col-md-12', 'text-center')}>
-                            <h2 className={styles.footerHeading}>
-                                Shoppii.com
-                            </h2>
-                            <ul className={clsx(styles.socialMedia)}>
-                                <li>
-                                    <Link to={'#'} className={styles.linkStyle}>
-                                        Home
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to={'#'} className={styles.linkStyle}>
-                                        AboutUs
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to={'#'} className={styles.linkStyle}>
-                                        Contact
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to={'#'} className={styles.linkStyle}>
-                                        Blog
-                                    </Link>
-                                </li>
-                            </ul>
-                            <ul className={clsx(styles.socialMedia)}>
-                                <li>
-                                    <Link to={'#'} className={styles.linkStyle}>
-                                        {<Twitter />}
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to={'#'} className={styles.linkStyle}>
-                                        {<FacebookRounded />}
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to={'#'} className={styles.linkStyle}>
-                                        {<Instagram />}
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to={'#'} className={styles.linkStyle}>
-                                        {<GitHub />}
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
+        <footer className={clsx(styles.body)}>
+            <div className="container">
+                <div className={clsx('row', 'justify-content-center')}>
+                    <div className={clsx('col-md-12', 'text-center')}>
+                        <h2 className={styles.footerHeading}>Shoppii.com</h2>
+                        <ul className={clsx(styles.socialMedia)}>
+                            <li>
+                                <Link to={'#'} className={styles.linkStyle}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'#'} className={styles.linkStyle}>
+                                    AboutUs
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'#'} className={styles.linkStyle}>
+                                    Contact
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'#'} className={styles.linkStyle}>
+                                    Blog
+                                </Link>
+                            </li>
+                        </ul>
+                        <ul className={clsx(styles.socialMedia)}>
+                            <li>
+                                <Link to={'#'} className={styles.linkStyle}>
+                                    {<Twitter />}
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'#'} className={styles.linkStyle}>
+                                    {<FacebookRounded />}
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'#'} className={styles.linkStyle}>
+                                    {<Instagram />}
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'#'} className={styles.linkStyle}>
+                                    {<GitHub />}
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </footer>
-        </div>
+            </div>
+        </footer>
     )
 }
 
