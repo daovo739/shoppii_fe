b26b0be5a23b943f85fc8e5ac3b14063f0860bf5
VVD <daovo739@gmail.com>
2022-10-22 22:03:51 +0700

    UI products shop, fix login Google

--- src/pages/user/Home/index.js
+++ src/pages/user/Home/index.js
@@ -1,10 +1,7 @@
 import { useState, useEffect } from 'react'
 import { get } from '../../../utils/./httprequest'
-<<<<<<< HEAD
 import Categories from './components/Categories'
 import Carousel from './components/Carousel'
-=======
->>>>>>> 52683f55894e4d8c4da12e2201e6b11c49cd72b2
 function Home() {
     const [category, setCategory] = useState([])
 
@@ -14,16 +11,12 @@ function Home() {
     //     }
     // }, [])
 
-<<<<<<< HEAD
     return (
         <div>
             <Carousel />
             <Categories />
         </div>
     )
-=======
-    return <h1>Home</h1>
->>>>>>> 52683f55894e4d8c4da12e2201e6b11c49cd72b2
 }
 
 export default Home
