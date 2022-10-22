0f0c2e291549b761f1d59c84d15f47a108193abe
VVD <daovo739@gmail.com>
2022-10-23 02:46:04 +0700

    fix

--- src/pages/user/Home/index.js
+++ src/pages/user/Home/index.js
@@ -1,13 +1,9 @@
 import { useState, useEffect } from 'react'
 import { get } from '../../../utils/./httprequest'
 import Categories from './components/Categories'
-<<<<<<< HEAD
 import ControlledCarousel from './components/Carousel'
 import { Container, Row, Col } from 'react-bootstrap'
 import FilterSidebar from '../../../components/FilterSidebar'
-=======
-import Carousel from './components/Carousel'
->>>>>>> 205d4fd951fd3ed8f395c4f1cbbde1c68fca522e
 function Home() {
     const [category, setCategory] = useState([])
 
