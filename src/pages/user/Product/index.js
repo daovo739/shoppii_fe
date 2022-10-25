3f4bef70768f804901868e68f502a5f63db6231f
VVD <daovo739@gmail.com>
2022-10-26 01:06:35 +0700

    get product detail

--- src/pages/user/Product/index.js
+++ src/pages/user/Product/index.js
@@ -1,4 +1,24 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import queryString from 'query-string'
+import { get } from '../../../utils/httprequest'
+
 function Product() {
+    const { id } = useParams()
+    const [product, setProduct] = useState({})
+
+    const getProduct = async () => {
+        const q = queryString.stringify({ productId: id })
+        const res = await get('/product', q)
+        const data = await res.json()
+        setProduct(data)
+    }
+    console.log(product)
+    useEffect(() => {
+        getProduct()
+    }, [id])
+
+    console.log(product)
     return <h1>Product detail page</h1>
 }
 
