01de96679498053a55ea908082e53c2dc3de484f
Hoai Nhan <hoainhan.lhp@gmail.com>
2022-10-28 18:48:27 +0700

    update address

--- src/pages/user/Profile/components/AddressList/index.js
+++ src/pages/user/Profile/components/AddressList/index.js
@@ -3,12 +3,41 @@ import './index.css'
 import AddressItem from '../Address'
 import AddressModal from '../AddressModal'
 
+const addressList = [
+    {
+        id: '1',
+        name: 'Ton That Khiem',
+        address:
+            '1x/45x đường Hoàng Diệu, phường Thạch Thang, Quận Hải Châu, Thành phố Đà Nẵng',
+        phone: '0159877459',
+    },
+    {
+        id: '2',
+        name: 'Vo Van Dao',
+        address:
+            '1x/45x đường Hoàng Diệu, phường Thạch Thang, Quận Hải Châu, Thành phố Đà Nẵng',
+        phone: '0159877459',
+    },
+    {
+        id: '3',
+        name: 'Ton That Khiem',
+        address:
+            '1x/45x đường Hoàng Diệu, phường Thạch Thang, Quận Hải Châu, Thành phố Đà Nẵng',
+        phone: '0159877459',
+    },
+]
+
 function AddressList() {
     return (
         <div className="address-list w-100 h-auto">
-            <AddressModal/>
-            {[0, 1, 2].map(item => (
-                <AddressItem key={item} />
+            <AddressModal test={false}/>
+            {addressList.map(address => (
+                <AddressItem
+                    key={address.id}
+                    name={address.name}
+                    address={address.address}
+                    phone={address.phone}
+                />
             ))}
         </div>
     )
