2635e400dc158d93bacfa28d02efb8357fd7b4ee
Hoai Nhan <hoainhan.lhp@gmail.com>
2022-10-31 22:00:24 +0700

    register shop UI

--- src/store/UserProvider.js
+++ src/store/UserProvider.js
@@ -1,6 +1,5 @@
 import UserContext from './UserContext'
 import { useReducer, useState } from 'react'
-<<<<<<< HEAD
 
 function UserProvider({ children }) {
     const [products, setProducts] = useState([])
@@ -11,21 +10,6 @@ function UserProvider({ children }) {
         setProducts,
         addresses,
         setAddresses
-=======
-import reducer, { initState } from './AddressHook/reducer'
-
-function UserProvider({ children }) {
-    const [productsData, setProductsData] = useState({})
-    const [state, dispatch] = useReducer(reducer, initState)
-
-    const value = {
-        productsData,
-        setProductsData,
-        addressHook: {
-            state,
-            dispatch,
-        },
->>>>>>> 23afe75d35b4df3f1b5f47a48c98f34ab5f17098
     }
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
