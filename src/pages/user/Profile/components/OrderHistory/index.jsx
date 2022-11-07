4529f6bed308f0e8a552eab30a4bfdaf16f8db29
Hoai Nhan <hoainhan.lhp@gmail.com>
2022-11-07 08:29:45 +0700

    order history - done

--- src/pages/user/Profile/components/OrderHistory/index.jsx
+++ src/pages/user/Profile/components/OrderHistory/index.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import './index.css'
 import OrderHistoryItem from "../OrderHistoryItem";
+import { get } from '../../../../../utils/httprequest'
+import queryString from 'query-string'
+import { handleFormData } from '../../../../../utils/handleForm'
+import { useAuth } from '../../../../../hooks/useAuth'
+import { ErrorSharp } from "@mui/icons-material";
+import { useEffect } from "react";
 
 function OrderHistory() {
+    const [orders, setOrders] = useState([])
+    const { user } = useAuth()
+    const getOrders = async () => {
+        const q = queryString.stringify({
+            userId: user.userId,
+            status: 'accepted'
+        })
+        const res = await get('order', q)
+        const data = await res.json()
+        console.log(data);
+        setOrders(data)
+    }
+
+    useEffect(() => {
+        getOrders()
+    },[])
+
     return (
         <div className="order-history w-100">
-            {[0,1,2].map(item => (
-                <OrderHistoryItem key={item}/>
+            {orders.map((order, index) => (
+                <OrderHistoryItem key={index} order={order}/>
             ))}
         </div>
     )
